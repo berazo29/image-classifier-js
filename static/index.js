84433d89649b594a381450ec1a7f7ee3a51c8c72
Bryan Erazo <bryanerazo29@gmail.com>
2021-12-22 05:11:58 -0500

    Implemented dynamic rendering of prediction data

--- static/index.js
+++ static/index.js
@@ -1,57 +1,55 @@
-// Upload the image
-const imgLoad = document.getElementById('file-1');
-const imgDisplay = document.querySelector('.previewImg');
-let imgVerification = false;
+const imgLoad = document.getElementById('file-1')
+const imgDisplay = document.querySelector('.previewImg')
+let imgVerification = false
 
-const ans = document.getElementById('answer');
+imgLoad.addEventListener('change', function () {
+  const file = this.files[0]
 
-imgLoad.addEventListener("change", function () {
-    const file = this.files[0];
+  if (file) {
+    const reader = new FileReader()
 
-    if (file) {
-        const reader = new FileReader();
+    reader.addEventListener('load', function () {
+      console.log(this)
+      imgDisplay.setAttribute('src', this.result)
+    })
+    reader.readAsDataURL(file)
+    imgVerification = true
+  }
+  if (imgVerification === true) {
+    console.log('Image Uploaded')
+    console.log('Reading...')
 
-        reader.addEventListener("load", function () {
-            console.log(this);
-            imgDisplay.setAttribute("src", this.result);
+    async function app () {
+      console.log('Loading mobilenet..')
 
-        });
-        reader.readAsDataURL(file);
-        imgVerification = true;
+      // Load the model.
+      net = await mobilenet.load()
+      console.log('Sucessfully loaded model')
+
+      // Make a prediction through the model on our image.
+      const imgEl = document.querySelector('.previewImg')
+      const result = await net.classify(imgEl)
+      const baseDiv = document.getElementById('base')
+      baseDiv.lastElementChild.setAttribute('id', 'id-0')
+      for (let i = 1; i < result.length; i++) {
+        const item = baseDiv.lastElementChild
+        const clone = item.cloneNode(true)
+        baseDiv.appendChild(clone)
+        baseDiv.lastElementChild.setAttribute('id', 'id-' + i.toString())
+      }
+      for (let i = 0; i < result.length; i++) {
+        const innerDiv = document.getElementById('id-' + i.toString())
+        innerDiv.lastElementChild.firstElementChild.setAttribute('id', 'description-' + i.toString())
+        innerDiv.lastElementChild.lastElementChild.setAttribute('id', 'percentage-' + i.toString())
+      }
+      result.forEach((e, index) => {
+        document.getElementById('description-' + index.toString()).innerHTML = e.className
+        const prob = (e.probability * 100).toFixed(2)
+        document.getElementById('percentage-' + index.toString()).innerHTML = prob.toString() + '%'
+      })
     }
-    if (imgVerification === true) {
-        console.log('Image Uploaded');
-        console.log('Reading...');
-
-        async function app() {
-            console.log('Loading mobilenet..');
-
-            // Load the model.
-            net = await mobilenet.load();
-            console.log('Sucessfully loaded model');
-
-            // Make a prediction through the model on our image.
-            const imgEl = document.querySelector('.previewImg');
-            const result = await net.classify(imgEl);
-            console.log(result);
-
-
-            // Change text
-            let roundProb = result[0].probability*100;
-            let textDis = result[0].className;
-            document.getElementById('answer').innerHTML = textDis;
-            document.getElementById('percentage').innerHTML = roundProb.toFixed(2)+'%';
-        }
-        app();
-
-    }
-    else {
-        console.log('No Image Uploaded')
-    }
-});
-
-
-//  EXPRESSION TO CHANCE THE TEXT
-/*
-    document.getElementById("p1").innerHTML = "New text!";
-*/
+    app().then(r => {})
+  } else {
+    console.log('No Image Uploaded')
+  }
+})
